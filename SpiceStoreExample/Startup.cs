f084bd5ee914d46c199b4cbf277c3d2068999c16
dmbalinskiy <dmbalinskiy@outlook.com>
2020-02-20 19:59:53 +0200

    added identity functionality, content management popup

--- SpiceStoreExample/Startup.cs
+++ SpiceStoreExample/Startup.cs
@@ -33,7 +33,7 @@ namespace SpiceStoreExample
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,9 +62,11 @@ namespace SpiceStoreExample
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
+            
         }
     }
 }
